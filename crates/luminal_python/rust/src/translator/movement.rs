rsoorajs/luminal

translator: robustness fixes — dtype promotion, rank-extending expand, norm opmath (#387)
Three translator-correctness fixes for legal graphs the lowering
mishandled, found working the top-100 sweep. +10 models, all verified
end-to-end on CUDA vs eager (and re-confirmed by a full 100-model
sweep: compile 60 -> 68, strict match 33 -> 42, zero regressions).

1. Mixed-dtype operand handling. ATen does type promotion inside
   kernels, so exported graphs legally carry mixed-dtype operands with
   no casts; the translator must promote before building core ops
   (which assert dtype equality). Two defects: ensure_same_dtype's
   lattice promoted (Int, I64) to Int, silently truncating the 64-bit
   side (comparisons past 2^31 flip); and the minimum/maximum/addmm
   arms never unified at all — core minimum() builds an lt()
   internally, so T5-family relative-position bucketing
   (torch.min(int64, int32)) panicked "Dtypes must match", blocking
   google-t5/t5-small and the chronos family since the first sweep.
   The lattice now mirrors torch.promote_types (see "Type promotion",
   https://docs.pytorch.org/docs/stable/tensor_attributes.html#type-promotion-doc):
   F64 > F32 > f16/bf16 (mixed 16-bit pair -> F32) > I64 > Int.
   -> t5-small, amazon/chronos-2, autogluon/chronos-2,
      chronos-bolt-small: panic -> match (maxdiff <= 2.3e-5).

2. Rank-extending expand. torch's expand PREPENDS new dims, so
   -1/existing sizes resolve right-aligned against the source shape;
   the lowering indexed left-aligned and walked off the end on CLIP's
   class_embedding.expand(B, 1, -1) ("index out of bounds"). Unsqueeze
   the new leading dims first (translate_repeat's idiom), then resolve
   against the rank-matched shape; equal-rank behavior is unchanged.
   Enforced per torch's contract: target rank >= source, -1 only on
   existing positions.
   -> all four CLIP checkpoints: panic -> match (cos 1.0).

3. layer_norm/group_norm statistics in fp32 (opmath). torch computes
   norm stat

crates/luminal_python/rust/src/translator/movement.rs
@@ -199,13 +199,17 @@ impl<'a> Translator<'a> {
     pub(crate) fn translate_expand(&mut self, node: &Node) -> Result<GraphTensor> {
         let mut a = self.get_input_tensor(node, 0)?;
         let neg1_expr = Expression::from(-1i32);
-        let target_shape: Vec<Expression> = if let Ok(sizes) = self.get_ints_arg(node, 1) {
+        // torch's expand PREPENDS new dims when the target rank exceeds the
+        // source rank, so `-1`/existing sizes resolve RIGHT-aligned against
+        // the source shape (`class_embedding.expand(B, 1, -1)`: 1-D -> 3-D).
+        // Unsqueeze leading dims first so the ShapeTracker expand sees
+        // matching ranks; left-aligned indexing walks off the source shape.
+        let raw: Vec<Expression> = if let Ok(sizes) = self.get_ints_arg(node, 1) {
             sizes
                 .iter()
-                .enumerate()
-                .map(|(i, &s)| {
+                .map(|&s| {
                     if s == -1 {
-                        a.shape.dims[i]
+                        neg1_expr
                     } else {
                         Expression::from(s as usize)
                     }
@@ -213,11 +217,32 @@ impl<'a> Translator<'a> {
                 .collect()
         } else {
             self.get_exprs_arg(node, 1)?
-                .into_iter()
-                .enumerate()
-                .map(|(i, e)| if e == neg1_expr { a.shape.dims[i] } else { e })
-                .collect()
         };
+        anyhow::ensure!(
+            raw.len() >= a.shape.len(),
+            "expand: target rank {} below source rank {}",
+            raw.len(),
+            a.shape.len()
+        );
+        let offset = raw.len() - a.shape.len();
+        for _ in 0..offset {
+            a = a.unsqueeze(0);
+        }
+        let target_shape: Vec<Expression> = raw
+            .into_iter()
+            .enumerate()
+            .map(|(i, e)| {
+                if e == neg1_expr {
+                    anyhow::ensure!(
+                        i >= offset,
+                        "expand: -1 is only valid for existing (right-aligned) dims"
+                    );
+                    Ok(a.shape.dims[i])
+                } else {
+                    Ok(e)
+                }
+            })
+            .collect::<Result<_>>()?;
         a.shape.expand(target_shape);
         Ok(a)
     }

feichai0017/orbitkv-luminal

(cherry picked from commit be3e2fe5372040064c7126fc7796cecef93add47)
Re-expressed onto logical-ssa-project: pt2_util.rs (promotion lattice),
dispatch.rs (addmm/minimum/maximum unification) and both test files applied
verbatim. movement.rs and unary.rs conflicted; in both, the BRANCH's
structure and API spellings were kept and main's new behaviour was
expressed against them.
- movement.rs translate_expand: main's rank-extending expand carried whole
  (raw target vector, `-1` left unresolved until after right-alignment, the
  rank ensure, the leading unsqueeze loop, and the `i >= offset` guard on
  `-1`), but the terminal `a.shape.expand(target_shape)` no longer exists —
  77d4123f destroyed ShapeTracker::expand — so it is re-expressed on the
  branch's replacement, `crate::pt2_util::tracker_expand(a
  .legacy_tracker_mut(), target_shape)` (the local fossil with the same
  broadcast semantics, prepending size-1 dims itself; after main's explicit
  unsqueeze loop the ranks already match, so its rank-extension arm is
  inert). Shape reads use the A2 accessor (`a.legacy_tracker_ref().len()` /
  `.dims[i]`) and `Expression` becomes `IntExpr`. The comment's "the
  ShapeTracker expand" is respelled "the tracker expand" to name what the
  code now calls.
- unary.rs translate_layer_norm / translate_group_norm: main's fp32 opmath
  is carried verbatim (`out_dtype`, `input.cast(DType::F32)`, F32 casts on
  weight/bias, `.cast(out_dtype)` at the end); the surrounding lines keep
  the branch's `t.legacy_tracker_ref().len()` and
  `expand_to_shape_on_axes(t.dims(), ..)`.
No branch rename was reverted and no main hunk was dropped.
Co-Authored-By: Claude Fable 5.1 <noreply@anthropic.com>

crates/luminal_python/rust/src/translator/movement.rs
@@ -198,13 +198,17 @@ impl<'a> Translator<'a> {
     pub(crate) fn translate_expand(&mut self, node: &Node) -> Result<GraphTensor> {
         let mut a = self.get_input_tensor(node, 0)?;
         let neg1_expr = IntExpr::from(-1i32);
-        let target_shape: Vec<IntExpr> = if let Ok(sizes) = self.get_ints_arg(node, 1) {
+        // torch's expand PREPENDS new dims when the target rank exceeds the
+        // source rank, so `-1`/existing sizes resolve RIGHT-aligned against
+        // the source shape (`class_embedding.expand(B, 1, -1)`: 1-D -> 3-D).
+        // Unsqueeze leading dims first so the tracker expand sees matching
+        // ranks; left-aligned indexing walks off the source shape.
+        let raw: Vec<IntExpr> = if let Ok(sizes) = self.get_ints_arg(node, 1) {
             sizes
                 .iter()
-                .enumerate()
-                .map(|(i, &s)| {
+                .map(|&s| {
                     if s == -1 {
-                        a.legacy_tracker_ref().dims[i]
+                        neg1_expr
                     } else {
                         IntExpr::from(s as usize)
                     }
@@ -212,11 +216,32 @@ impl<'a> Translator<'a> {
                 .collect()
         } else {
             self.get_exprs_arg(node, 1)?
-                .into_iter()
-                .enumerate()
-                .map(|(i, e)| if e == neg1_expr { a.legacy_tracker_ref().dims[i] } else { e })
-                .collect()
         };
+        anyhow::ensure!(
+            raw.len() >= a.legacy_tracker_ref().len(),
+            "expand: target rank {} below source rank {}",
+            raw.len(),
+            a.legacy_tracker_ref().len()
+        );
+        let offset = raw.len() - a.legacy_tracker_ref().len();
+        for _ in 0..offset {
+            a = a.unsqueeze(0);
+        }
+        let target_shape: Vec<IntExpr> = raw
+            .into_iter()
+            .enumerate()
+            .map(|(i, e)| {
+                if e == neg1_expr {
+                    anyhow::ensure!(
+                        i >= offset,
+                        "expand: -1 is only valid for existing (right-aligned) dims"
+                    );
+                    Ok(a.legacy_tracker_ref().dims[i])
+                } else {
+                    Ok(e)
+                }
+            })
+            .collect::<Result<_>>()?;
         crate::pt2_util::tracker_expand(a.legacy_tracker_mut(), target_shape);
         Ok(a)
     }